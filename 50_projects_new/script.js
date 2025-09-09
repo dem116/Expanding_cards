4deb88981dc434f921719509dd00a1237359537c
Damian Escobar <damianem611@gmail.com>
2025-09-09 13:39:03 +0200

    modificado el tipo de expansion de cada carta respect a las otras

--- 50_projects_new/script.js
+++ 50_projects_new/script.js
@@ -6,7 +6,15 @@ const cards = document.querySelectorAll('.card');
 function expandCard() {
     cards.forEach(card => {
         card.addEventListener('click', () => {
-            card.classList.toggle('cardExpanded');
+            cards.forEach(c => {
+                c.classList.remove('cardExpanded', 'cardReduced');
+            });
+            card.classList.add('cardExpanded');
+            cards.forEach(c => {
+                if (c !== card) {
+                    c.classList.add('cardReduced');
+                }
+            });
         });
     });
 }
